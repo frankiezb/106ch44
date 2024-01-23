3294ed167dfa01491dd30343fd6fe52130a06888
frankiezb <donald.francois001@gmail.com>
2024-01-22 23:40:41 -0600

    assignment 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,34 @@
-function sayHello(name, lastName) {
-    console.log("Hello " + name + " " + lastName); // Child
+//global variables
+let iconImportant = false;
+function saveTask(){
+    console.log("Saving task");
 }
+function changeIcon(){
+    console.log("Changing icon");
+    const nonImportant = "fa-solid fa-phone";
+    const isImportant = "fa-solid fa-bars";
+    if (iconImportant) { 
 
-function init() {
-    console.log('Hello world');
-    sayHello("Donald", "Francois");
-    sayHello("John", "Doe"); // Child/parent
-    const result = sum(21, 21);
-    console.log(result);
-    printNumbers();
-}
-
-window.onload = init;// parent becuase disreputor
-
-function sum(num1, num2) {
-    const res = num1 + num2;
-    return res;
-}
-
-function printNumbers() {
-    // Print numbers from 1 to 20 except 7 and 13
-    let sum = 0;
-    let count = 0;
-
-    let numbers = [
-        12, 4, 123, 4567, 234, 56, 12, 87, 124, 865, 233, 788, 43, 91, 544, 782, 653, 845,
-    ];
-
-    for (let i = 1; i < 21; i++) {
-        if (i !== 7 && i !== 13) { // Changed != to !== for strict equality
-            console.log(i);
-        }
+    $("#iImportant").removeClass(isImportant)("#nonImportant").addClass(nonImportant);
+    iconImportant = false;
     }
-
-    // Create a function that sums two numbers and execute it in the init
-    for (let i = 0; i < numbers.length; i++) {
-        let num = numbers[i];
-        console.log(num);
-        // Sum numbers
-        sum += num;
-        // Count only greater numbers
-        if (num > 500) {
-            count += 1;
-        }
+    else{
+    $("#iImportant").removeClass(nonImportant)("#nonImportant").addClass(isImportant);
+    iconImportant = true;
     }
-    console.log("The sum of the numbers is: " + sum);
-    console.log("There are " + count + " numbers bigger than 500");
+    $("#iconImportant")
+    //please try to restore the icon
 }
+
+function init(){
+console.log("this is the parent of everything")
+//load data
+
+//hook events
+$("#btnSave").click(saveTask);
+$("#iImportant").click(changeIcon);
+//document.getelementbyid("btnSave").click(saveTask)
+}
+
+
+window.onload = init;
